20ecc404ae2e53c18824126f59e064015462e585
Bhaskar Pandey <71315985+chikkibum@users.noreply.github.com>
2024-06-07 22:19:54 +0530

    proper formatting

--- src/components/Tshirt.tsx
+++ src/components/Tshirt.tsx
@@ -1,36 +1,37 @@
 /* eslint-disable @next/next/no-img-element */
-import { cn } from "@/lib/utils"
-import { HTMLAttributes } from "react"
+import { cn } from "@/lib/utils";
+import { HTMLAttributes } from "react";
 
 //
 interface TshirtProps extends HTMLAttributes<HTMLDivElement> {
-    imgSrc: string
-    dark?: boolean
+  imgSrc: string;
+  dark?: boolean;
 }
 
-const Tshirt = ({className, imgSrc, dark = false, ...props}: TshirtProps) => {
+const Tshirt = ({ className, imgSrc, dark = false, ...props }: TshirtProps) => {
   return (
-    <div className={cn(
+    <div
+      className={cn(
         "relative pointer-events-none z-50 overflow-hidden",
-        className)}
-        {...props}>
-            <img src={
-                dark
-                ? '/tshirt-template-dark-edges.png'
-                : '/t.png'
-            }
-            className="pointer-events-none z-50 size-96 object-cover p-2 select-none" 
-            alt="Tshirt-template" 
-            />
+        className
+      )}
+      {...props}
+    >
+      <img
+        src={dark ? "/tshirt-template-dark-edges.png" : "/t.png"}
+        className="pointer-events-none z-50 size-96 object-cover p-2 select-none"
+        alt="Tshirt-template"
+      />
 
-          <div className="size-20 absolute mt-5 mr-2 z-10 right-36 top-28">
-                <img src={imgSrc} 
-                className="object-cover min-w-full min-h-full" 
-                alt="Tshirt image"
-                 />
-          </div>
+      <div className="size-20 absolute mt-5 mr-2 z-10 right-36 top-28">
+        <img
+          src={imgSrc}
+          className="object-cover min-w-full min-h-full"
+          alt="Tshirt image"
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Tshirt
+export default Tshirt;
